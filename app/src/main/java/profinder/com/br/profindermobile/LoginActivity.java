6b32c065967e1929bbd2ad6ca0f81613c64cbade
covadevs <covadevs@gmail.com>
2018-04-26 15:03:24 -0300

    Inicio da implementacao de autenticacao de usuario, criacao dasviews da tela de cadastro de usuario e inicio da configuracao da splashscreen

--- app/src/main/java/profinder/com/br/profindermobile/LoginActivity.java
+++ app/src/main/java/profinder/com/br/profindermobile/LoginActivity.java
@@ -1,27 +1,74 @@
 package profinder.com.br.profindermobile;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText mUsuario,mSenha;
+    private TextView tvSignUp;
     private Button mLogin;
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         mUsuario = findViewById(R.id.usuario);
         mSenha = findViewById(R.id.senha);
+        tvSignUp = findViewById(R.id.tv_cadastre_se);
         mLogin = findViewById(R.id.buttonlogin);
+        mAuth = FirebaseAuth.getInstance();
 
         mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                signIn(mUsuario.getText().toString(), mSenha.getText().toString());
+            }
+        });
 
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        user = mAuth.getCurrentUser();
+        if(user != null) {
+            Intent intent = new Intent(LoginActivity.this, ProfessorActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    private void signIn(String usuario, String senha) {
+        mAuth.signInWithEmailAndPassword(usuario, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    user = mAuth.getCurrentUser();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
